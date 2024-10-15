5b5d87b109208fecfeb4bbb859e122d3a853380f
Iscotech <iscotech594@gmail.com>
2024-10-15 05:59:23 +0100

    First Commit

---
+++ src/App.jsx
@@ -0,0 +1,38 @@
+import { BrowserRouter as Router } from "react-router-dom";
+import {
+  About,
+  Contact,
+  Experience,
+  Feedbacks,
+  Hero,
+  Navbar,
+  Tech,
+  Works,
+  StarsCanvas,
+} from "./components";
+import { Toaster } from "react-hot-toast";
+
+const App = () => {
+  return (
+    <Router>
+      <Toaster />
+      <div className="relative z-0 bg-primary">
+        <div className="bg-hero-pattern bg-cover bg-no-repeat bg-center">
+          <Navbar />
+          <Hero />
+        </div>
+        <About />
+        <Experience />
+        <Tech />
+        <Works />
+        <Feedbacks />
+        <div className="relative z-0">
+          <Contact />
+          <StarsCanvas />
+        </div>
+      </div>
+    </Router>
+  );
+};
+
+export default App;
