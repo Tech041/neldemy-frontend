3960843d98e90eb965941321561dc9df05f16245
nelson <chinwubanelson@gmail.com>
2025-01-31 20:12:55 +0100

    completed course-list page

--- src/components/student/CourseSection.jsx
+++ src/components/student/CourseSection.jsx
@@ -1,7 +1,7 @@
 import { useContext } from "react";
 import { Link } from "react-router-dom";
 import { AppContext } from "../../context/AppContext";
-import CoureCard from "./CoureCard";
+import CoureCard from "./CourseCard";
 
 const CourseSection = () => {
   const { allCourses } = useContext(AppContext);
