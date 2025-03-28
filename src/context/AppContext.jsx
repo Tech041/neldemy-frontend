638b59c1304726780feef5c6d2abb75eadbf10b8
nelson <chinwubanelson@gmail.com>
2025-03-28 08:02:16 +0100

    added sample env

--- src/context/AppContext.jsx
+++ src/context/AppContext.jsx
@@ -2,6 +2,7 @@ import { createContext, useEffect, useState } from "react";
 import { dummyCourses } from "../assets/assets";
 import { useNavigate } from "react-router-dom";
 import humanizeDuration from "humanize-duration";
+import { useAuth, useUser } from "@clerk/clerk-react";
 export const AppContext = createContext();
 export const AppContextProvider = (props) => {
   const [allCourses, setAllCourses] = useState([]);
@@ -10,6 +11,8 @@ export const AppContextProvider = (props) => {
 
   const currency = import.meta.env.VITE_CURRENCY;
   const navigate = useNavigate();
+  const { getToken } = useAuth();
+  const { user } = useUser();
 
   // fetch user enrolled courses
   const fetchUserEnrolledCourses = async () => {
@@ -23,6 +26,16 @@ export const AppContextProvider = (props) => {
     fetchAllCourses();
     fetchUserEnrolledCourses();
   }, []);
+
+  // Getting token
+  const logToken = async () => {
+    console.log(await getToken());
+  };
+  useEffect(() => {
+    if (user) {
+      logToken();
+    }
+  }, [user]);
   // Function to calculate average course rating
 
   const calculateRating = (course) => {
